d5746d7702ec5cb65158416b3cc90d1aaa5be6e1
Lina Florin <lina@plakos.de>
2024-06-05 17:45:41 +0200

    trying to change overview

--- js/overview.js
+++ js/overview.js
@@ -31,15 +31,16 @@
  * @param {type} __deck
  * @returns {undefined}
  */
- function startOverview(Y, __cmid, __topic, __sort, __deck) { // Wrapper function that is called by controller.php.
+ function startOverview(Y, __cmid, __sort, __deck) { // Wrapper function that is called by controller.php.
 
     require(['jquery', 'core/notification'], function ($, notification) {
-        var topicfilter = document.getElementById('cardbox-overview-topicfilter');
+        // var topicfilter = document.getElementById('cardbox-overview-topicfilter');
         var filterselect = document.getElementById('cardbox-filter-options');
         var deckfilter = document.getElementById('cardbox-overview-deckfilter')
 
         document.getElementById('cardbox-filter-options').value = __sort;        
 
+        /*
         topicfilter.onchange = function() {
 
             var select = this.options[this.selectedIndex];        
@@ -47,19 +48,19 @@
             window.location.href = window.location.pathname + '?id=' + __cmid + '&action=overview&topic=' + __topic + '&sort=' + __sort + '&deck=' + __deck;
 
         }
+        */
+
         deckfilter.onchange = function() {
 
-            var select = this.options[this.selectedIndex];        
+            var select = this.options[this.selectedIndex];
             __deck = select['value'];
-            window.location.href = window.location.pathname + '?id=' + __cmid + '&action=overview&topic=' + __topic + '&sort=' + __sort + '&deck=' + __deck;
-
+            window.location.href = window.location.pathname + '?id=' + __cmid + '&action=overview&sort=' + __sort + '&deck=' + __deck;
         }
         filterselect.onchange = function() {
 
             var select = this.options[this.selectedIndex];        
             __sort = select['value'];
-            window.location.href = window.location.pathname + '?id=' + __cmid + '&action=overview&topic=' + __topic + '&sort=' + __sort + '&deck=' + __deck;
-
+            window.location.href = window.location.pathname + '?id=' + __cmid + '&action=overview&sort=' + __sort + '&deck=' + __deck;
         }
         const editbtns = document.querySelectorAll('#cardbox-overview .cardbox-overview-button-edit');
         editbtns.forEach(btn => {
